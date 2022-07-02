f7e8c65f120337689a688f9d78c9feca3f7d4a99
angeleri.0206 <angeleri.0206@gmail.com>
2022-07-02 19:45:10 +0200

    Aggiunta al file main.c nella funzione print_top la porzione di codice per la stampa delle informazioni di ogni processo e con la funzionalità di scansionare tutte le directory dei vari processi; presente anche la porzione di codice per il calcolo della cpu utilizzata e della memoria utilizzata. Per le fonti delle formule utilizzate controllare gli useful links presenti nel codice.

--- main.c
+++ main.c
@@ -10,9 +10,15 @@ void sig_handler(int signum) {
     print = 1;
 }
 
+//aa
+int filter(const struct dirent *dir) {
+    return !fnmatch("[1-9]*",dir->d_name,0);
+}
+
 
 /* Useful links:
- * https://linuxaria.com/howto/understanding-the-top-command-on-linux?lang=it */
+ * https://linuxaria.com/howto/understanding-the-top-command-on-linux?lang=it ,  
+   https://www.baeldung.com/linux/total-process-cpu-usage */
 void print_top() {
     write(STDOUT_FILENO, "\e[1;1H\e[2J", 11); 
 
@@ -29,6 +35,39 @@ void print_top() {
             MiB(mem->swapFree), 
             MiB((mem->swapTotal - mem->swapFree)), 
             MiB(mem->memAvailable));
+
+    //aa
+    //CICLO SUI PID
+    printf("\n%c[%d;%dmPID       |USER  |PR   |NI   |VIRT      |RES  |SH |S    |%%CPU |%%MEM |TIME+     |COMMAND%c[%dm\n",27,1,35,27,0);
+    struct dirent **namelist;
+    ProcInfo *proc = (ProcInfo*)malloc(sizeof(ProcInfo));
+    //prendiamo dalla directory proc solo le directory il cui nome sia un numero
+    //in modo da prendere solo le cartelle dei processi
+    int n = scandir("/proc",&namelist,filter,alphasort);
+    if (n == -1) {
+        perror("scandir");
+        exit(EXIT_FAILURE);
+    }
+    while (n--) {
+        //richiama la funzione get_procinfo su ogni processo
+        get_procinfo(proc,atoi(namelist[n]->d_name));
+        printf("%-10d| user |%-5ld|%-5ld|%-10lu|%-5ld|SHR|%-5c|%-5.1f|%-5.1f|0:%05.2f|%s\n",
+                proc->pid,
+                proc->priority,
+                proc->nice,
+                (long int) MiB(proc->virt),
+                proc->res,
+                proc->state,
+                (float)  ( ((proc->utime/100) + (proc->stime/100))*100 / (uptime - (proc->starttime / 100)) ),
+                (float) ((proc->statm_resident + proc->statm_data)*100) / (mem->memTotal),
+                (float) (proc->utime + proc->stime) / 100,
+                proc->command);
+        free(proc);
+        free(loads);
+        free(mem);
+
+        alarm(1);        
+    }
 
 
 
