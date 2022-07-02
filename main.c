bc8b268935a2d3cb8df2e5d9e7535540c4a02333
Luigi Gallo <luigi.gallo.uni@gmail.com>
2022-07-02 14:38:42 +0200

    first print_top: print Mem stats

--- main.c
+++ main.c
@@ -13,6 +13,27 @@ void sig_handler(int signum) {
 
 /* Useful links:
  * https://linuxaria.com/howto/understanding-the-top-command-on-linux?lang=it */
+void print_top() {
+    write(STDOUT_FILENO, "\e[1;1H\e[2J", 11); 
+
+    MemInfo *mem = (MemInfo*)malloc(sizeof(MemInfo));
+    get_meminfo(mem);
+    printf("MiB Mem: %.1f total, %.1f free, %.1f used, %.1f buff/cache\n", 
+            MiB(mem->memTotal), 
+            MiB(mem->memFree), 
+            MiB((mem->memTotal - mem->memFree)), 
+            MiB(mem->buffers));
+    printf("MiB Swap: %.1f total, %.1f free, %.1f used, %.1f avail Mem\n", 
+            MiB(mem->swapTotal), 
+            MiB(mem->swapFree), 
+            MiB((mem->swapTotal - mem->swapFree)), 
+            MiB(mem->memAvailable));
+
+    free(mem);
+    
+    alarm(1);
+}
+
 
 int main() {
     signal(SIGALRM, sig_handler); // Register signal handler
@@ -20,7 +41,8 @@ int main() {
     while(1) {
         if(print) {
             print = 0;
-            // print top
+            print_top();
         } 
     }
+    return 0;
 }
